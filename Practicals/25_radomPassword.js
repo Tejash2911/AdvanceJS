756eff0e921c72bfc848309dcdcaf1c888fd67c1
Tejash Patel <108259942+Tejash2911@users.noreply.github.com>
2023-08-20 16:48:40 +0530

    Add files via upload

---
+++ Practicals/25_radomPassword.js
@@ -0,0 +1,18 @@
+// program to generate random password with a specified length
+
+function generateRandomPassword(length) {
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let password = "";
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+
+    return password;
+}
+
+// Example usage:
+const passwordLength = 10;
+const randomPassword = generateRandomPassword(passwordLength);
+console.log(randomPassword);
