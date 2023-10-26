5c286a7d3eb0edf110defffa98dacb88bd10047d
Tejash Patel <108259942+Tejash2911@users.noreply.github.com>
2023-10-26 23:46:22 +0530

    Add files via upload

---
+++ Practicals/77_findMaxSumOfSubArr.js
@@ -0,0 +1,18 @@
+// Implement a function to find the maximum sum of a subarray in an array of numbers(Kadane's algorithm).
+
+function maxSubarraySum(arr) {
+    let maxEndingHere = arr[0];
+    let maxSoFar = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        maxEndingHere = Math.max(arr[i], maxEndingHere + arr[i]);
+        maxSoFar = Math.max(maxSoFar, maxEndingHere);
+    }
+
+    return maxSoFar;
+}
+
+// Example usage:
+const array = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+const maxSum = maxSubarraySum(array);
+console.log(maxSum); // Output: 6 (The maximum sum subarray is [4, -1, 2, 1])
