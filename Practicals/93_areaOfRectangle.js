731d03906498a5136a7349cc7862443082d5ad98
Tejash Patel <108259942+Tejash2911@users.noreply.github.com>
2023-11-12 22:08:49 +0530

    Add files via upload

---
+++ Practicals/93_areaOfRectangle.js
@@ -0,0 +1,12 @@
+// Create a function to calculate the area of a rectangle given its length and width.
+
+function calculateRectangleArea(length, width) {
+    return length * width;
+}
+
+// Example usage:
+const rectangleLength = 5;
+const rectangleWidth = 10;
+const area = calculateRectangleArea(rectangleLength, rectangleWidth);
+console.log(area); // Output: 50
+
